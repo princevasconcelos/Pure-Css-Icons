f415a8363b11801342522969ec42b788435b50c3
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:29:13 -0300

    trapezoid

---
+++ components/Trapezoid.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Trapezoid = styled.div`
+  width: 100px;
+  height: 0;
+
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-bottom: 50px solid crimson;
+`;
+
+export default Trapezoid;
