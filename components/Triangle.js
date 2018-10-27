8d93972901e3930467e322c3c8b38df74dfa0df9
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:29:21 -0300

    triangle

---
+++ components/Triangle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Triangle = styled.div`
+  width: 0;
+  height: 0;
+  border-right: 50px solid transparent;
+  border-bottom: 100px solid crimson;
+  border-left: 50px solid transparent;
+`;
+
+export default Triangle;
