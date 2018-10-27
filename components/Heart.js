d591d476b5139aa2ca32c1de833fa1a50c8406d2
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:28:00 -0300

    heart

---
+++ components/Heart.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Heart = styled.div`
+  position: relative;
+  width: 100px;
+  height: 50px;
+`;
+
+export default Heart;
