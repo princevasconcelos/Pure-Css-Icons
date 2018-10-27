9b384c8d85584ea825a0c7937c4ce5e268e7a89b
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:28:21 -0300

    oval

---
+++ components/Oval.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Oval = styled.div`
+  width: 200px;
+  height: 100px;
+  background-color: crimson;
+  border-radius: 50%;
+`;
+
+export default Oval;
