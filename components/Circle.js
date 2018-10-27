a7f640a30d83a3ce5b253e6a881528c62ba9d772
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:27:31 -0300

    circle

---
+++ components/Circle.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Circle = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: crimson;
+  border-radius: 50%;
+`;
+
+export default Circle;
