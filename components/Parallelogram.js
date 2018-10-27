d179c7fb99a446fac13bbd349745adc0466d95a5
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:28:31 -0300

    parallelogram

---
+++ components/Parallelogram.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Parallelogram = styled.div`
+  width: 150px;
+  height: 100px;
+
+  background-color: crimson;
+
+  transform: skew(20deg);
+`;
+
+export default Parallelogram;
