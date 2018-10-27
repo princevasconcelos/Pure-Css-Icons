b2424724c666f3703aa19474bfbb3c275b54ef07
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:29:05 -0300

    square

---
+++ components/Square.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Square = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: crimson;
+`;
+
+export default Square;
