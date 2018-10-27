833c7ff6b9505b0ee028bb312c8de8dd5b0eb2a6
Joaquim <joaquim@princevasconcelos.local>
2018-10-27 01:28:49 -0300

    rectangle

---
+++ components/Rectangle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rectangle = styled.div`
+  width: 200px;
+  height: 100px;
+  background-color: crimson;
+`;
+
+export default Rectangle;
